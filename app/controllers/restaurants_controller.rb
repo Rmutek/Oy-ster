2a1531d4ef916860ee25a231d97d45a1b5d57577
Rebecca Mutek <mutek.b@gmail.com>
2017-04-09 16:44:57 -0500

    added index and show action and views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,12 @@
+class RestaurantsController < ApplicationController
+  
+  def index 
+    @restaurants = Restaurant.all 
+    render "index.html.erb"
+  end 
+
+  def show
+    @restaurant = Restaurant.find_by(id: params[:id])
+    render "show.html.erb"
+  end 
+end
