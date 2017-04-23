a551a58dc2b4802cdee2809f5b09fde23dc610ac
Rebecca Mutek <mutek.b@gmail.com>
2017-04-23 15:13:41 -0500

    adding form for new review

--- app/controllers/oyster_reviews_controller.rb
+++ app/controllers/oyster_reviews_controller.rb
@@ -1,14 +1,22 @@
 class OysterReviewsController < ApplicationController
+  def index 
+    @reviews = Review.all 
+  end 
+
+  def new 
+    render "new.html.erb"
+  end 
+
   def create 
-    review = Review.new(
+    @review = Review.new(
       title: params[:title], 
       body: params[:body], 
       oyster_quality: params[:oyster_quality], 
       shucking_quality: params[:shucking_quality], 
       presentation: params[:presentation], 
       user_id: current_user.id,
-      resturant_id:, 
-      oyster_id: 
+      # resturant_id:, 
+      # oyster_id: 
       )
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   get "/restaurants" => "restaurants#index"
   get "restaurants/:id" => "restaurants#show"
 
+  get "/newreview" =>"oyster_reviews#new"
+  post "/oysterreviews" => "oyster_reviews#create"
+
   get "/signup" => "users#new"
   post "/users" => "users#create"
-  
+
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
