0681cee784dcb8bcdbfae27fb2aac6451894a583
Rebecca Mutek <mutek.b@gmail.com>
2017-10-01 13:25:06 -0500

    updated bundler

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module Capstone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-
+  
+  config.serve_static_assets = true
 end
 
-config.serve_static_assets = true
+
